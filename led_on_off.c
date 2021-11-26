cf6a272a237ed27eb4004bf2020d1dcf8a6cf019
Programmer-RD-AI <aigithub@gmail.com>
2021-11-26 13:47:02 +0530

    Update led_on_off.c

--- led_on_off.c
+++ led_on_off.c
@@ -6,8 +6,26 @@ void setup()
 }
 void loop()
 {
-    int delayPeriod = 100;      // In ms
+    static int delayPeriod = 100; // In ms
+    static int countDir = 1;
     digitalWrite(ledPin, HIGH); // Turn on the LED with HIGH output V
-    delay(delayPeriod);
-    digitalWrite(ledPin, LOW); // Turn off the LED with LOW output V
+    delay(delayPeriod);         // Delay the amount of delayPeriod
+    digitalWrite(ledPin, LOW);  // Turn off the LED with LOW output V
+    delay(delayPeriod);         // Delay the amount of delayPeriod
+    countDir = checkDirChange(delayPeriod, countDir);
+}
+int checkDirChange(int delayPeriod, int countDir)
+{
+    if ((delayPeriod == 1000) || (delayPeriod == 0))
+    {
+        countDir *= -1;
+        if (countDir < 0)
+        {
+            Serial.println("Going Down");
+        }
+        else
+        {
+            Serial.println("Going Up");
+        }
+    }
 }
